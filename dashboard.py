6a181dc2de6f494244dff934dc92815424cc003a
Abel Mekuria <abelmekuria46@gmail.com>
2026-03-10 13:41:15 -0400

    Fix SQLAlchemy text import

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
 import plotly.express as px
 import google_reviews_scraper
